d2b5f738c41ab7883a41f706e09e83404531fe6e
krugru7-alt <krugru7@gmail.com>
2026-08-21 11:37:27 +0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Для Кэссички ❤️",
+  description: "for you",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  );
+}
