a1036abd266d019e1166474f58ae2c6c0cafc2f1
krugru7-alt <krugru7@gmail.com>
2026-08-26 17:10:15 +0500

    Create route.js

---
+++ app/api/auth/logout/route.js
@@ -0,0 +1,21 @@
+import {
+  clearSession,
+} from "../../../lib/auth";
+
+
+export const runtime =
+  "nodejs";
+
+
+export async function POST() {
+
+  await clearSession();
+
+
+  return Response.json({
+
+    ok:
+      true,
+
+  });
+}
